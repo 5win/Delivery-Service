740d3e80262ded3fbd466986790f3e94b926bb90
5win <94297900+5win@users.noreply.github.com>
2024-12-07 17:54:41 +0900

    [#1] test: 구현할 기능 TDD 명세

---
+++ src/test/java/com/oheat/shop/CategoryCRUDTest.java
@@ -0,0 +1,54 @@
+package com.oheat.shop;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.oheat.shop.entity.CategoryJpaEntity;
+import com.oheat.shop.exception.DuplicateCategoryException;
+import com.oheat.shop.repository.CategoryRepository;
+import com.oheat.shop.repository.ShopRepository;
+import com.oheat.shop.service.CategoryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CategoryCRUDTest {
+
+    private CategoryService categoryService;
+    private final ShopRepository memoryShopRepository = new MemoryShopRepository();
+    private final CategoryRepository memoryCategoryRepository = new MemoryCategoryRepository();
+
+    @BeforeEach
+    void setUp() {
+        categoryService = new CategoryService(memoryCategoryRepository);
+    }
+
+    @Test
+    @DisplayName("카테고리명이 중복되지 않으면 카테고리 추가 성공")
+    void categoryNameNotDuplicate_thenSuccess() {
+        assertDoesNotThrow(() -> {
+            categoryService.registerCategory("치킨");
+        });
+    }
+
+    @Test
+    @DisplayName("카테고리명이 중복되면 카테고리 추가 실패")
+    void categoryNameDuplicate_thenFail() {
+        memoryCategoryRepository.save(CategoryJpaEntity.builder()
+            .name("치킨")
+            .build());
+
+        assertThrows(DuplicateCategoryException.class, () -> {
+            categoryService.registerCategory("치킨");
+        });
+    }
+
+    @Disabled
+    @Test
+    @DisplayName("3개의 카테고리를 저장하고 카테고리 목록을 조회하면, 목록의 size가 3이어야 함")
+    void givenThreeCategories_whenFindAll_thenListSizeThree() {
+
+    }
+
+}
--- src/test/java/com/oheat/shop/ShopCRUDTest.java
+++ src/test/java/com/oheat/shop/ShopCRUDTest.java
@@ -4,10 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.oheat.shop.dto.ShopSaveRequest;
-import com.oheat.shop.entity.CategoryJpaEntity;
 import com.oheat.shop.entity.ShopJpaEntity;
 import com.oheat.shop.exception.CategoryNotExistsException;
-import com.oheat.shop.exception.DuplicateCategoryException;
 import com.oheat.shop.exception.DuplicateShopNameException;
 import com.oheat.shop.repository.CategoryRepository;
 import com.oheat.shop.repository.ShopRepository;
@@ -18,7 +16,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class ShopServiceTest {
+public class ShopCRUDTest {
 
     private ShopService shopService;
     private CategoryService categoryService;
@@ -63,26 +61,6 @@ public class ShopServiceTest {
     }
 
     @Test
-    @DisplayName("카테고리명이 중복되지 않으면 카테고리 추가 성공")
-    void categoryNameNotDuplicate_thenSuccess() {
-        assertDoesNotThrow(() -> {
-            categoryService.registerCategory("치킨");
-        });
-    }
-
-    @Test
-    @DisplayName("카테고리명이 중복되면 카테고리 추가 실패")
-    void categoryNameDuplicate_thenFail() {
-        memoryCategoryRepository.save(CategoryJpaEntity.builder()
-            .name("치킨")
-            .build());
-
-        assertThrows(DuplicateCategoryException.class, () -> {
-            categoryService.registerCategory("치킨");
-        });
-    }
-
-    @Test
     @DisplayName("카테고리가 null이면 매장 등록 실패")
     void whenCategoryIsNull_thenFail() {
         assertThrows(RuntimeException.class, () -> {
@@ -113,4 +91,66 @@ public class ShopServiceTest {
 
     }
 
+    @Disabled
+    @Test
+    @DisplayName("치킨 매장 3개를 등록하고 치킨 카테고리에 해당하는 매장을 조회하면, 리스트 size가 3이어야 함")
+    void givenThreeChickenShops_whenFindAllChickenShop_thenListSizeThree() {
+
+    }
+
+    @Disabled
+    @Test
+    @DisplayName("카테고리로 매장 목록 조회 시, 각 매장의 상호명, 배달팁, 최소주문금액 정보 표시")
+    void whenFindChickenShop_thenReturnTitleAndDeliveryTipAndMinimumOrderAmount() {
+
+    }
+
+    @Disabled
+    @Test
+    @DisplayName("카테고리로 매장 목록 조회 시 정렬 기준이 기본순이면, 최근 등록 순으로 조회")
+    void whenSortByDefault_thenRecentRegistrationOrder() {
+
+    }
+
+    @Disabled
+    @Test
+    @DisplayName("카테고리로 매장 목록 조회 시 정렬 기준이 배달팁 낮은 순이면, 배달팁 오름차순으로 조회")
+    void whenSortByDeliveryTip_thenDeliveryTipAscendingOrder() {
+
+    }
+
+    @Disabled
+    @Test
+    @DisplayName("카테고리로 매장 목록 조회 시 정렬 기준이 최소주문 금액 낮은 순이면, 최소주문 금액 오름차순으로 조회")
+    void whenSortByMinimumOrderAmount_thenMinimumOrderAmountAscendingOrder() {
+
+    }
+
+    @Disabled
+    @Test
+    @DisplayName("카테고리로 매장 목록 조회 시 정렬 기준이 가까운 순이면, 내 위치에서의 거리 오름차순으로 조회")
+    void whenSortByNearestOrder_thenDistanceInMyLocationAscendingOrder() {
+
+    }
+
+    @Disabled
+    @Test
+    @DisplayName("카테고리로 매장 목록 조회 시 정렬 기준이 주문 많은 순이면, 누적 주문 수 내림차순으로 조회")
+    void whenSortByOrderAmount_thenOrderAmountDescendingOrder() {
+
+    }
+
+    @Disabled
+    @Test
+    @DisplayName("카테고리로 매장 목록 조회 시 정렬 기준이 별점 높은 순이면, 별점 내림차순으로 조회")
+    void whenSortByReviewScore_thenReviewScoreDescendingOrder() {
+
+    }
+
+    @Disabled
+    @Test
+    @DisplayName("매장 상세 조회 시, 상호명, 최소주문 금액, 전화번호, 배달팁, 메뉴명, 메뉴 설명, 메뉴 가격 조회")
+    void whenFindShop_thenReturnDetailInfo() {
+
+    }
 }
