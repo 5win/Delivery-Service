eff455c9132af1b88e9f44699c2e52e548bd3e57
5win <94297900+5win@users.noreply.github.com>
2024-12-08 15:59:56 +0900

    [#1] test: JPA 엔티티 비교 정확도 관련 테스트 명세

--- src/test/java/com/oheat/shop/ShopRepositoryTest.java
+++ src/test/java/com/oheat/shop/ShopRepositoryTest.java
@@ -9,6 +9,7 @@ import com.oheat.shop.entity.ShopJpaEntity;
 import com.oheat.shop.repository.CategoryJpaRepository;
 import com.oheat.shop.repository.ShopJpaRepository;
 import java.util.List;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -97,5 +98,12 @@ public class ShopRepositoryTest {
         List<ShopJpaEntity> result = shopJpaRepository.findByCategory(category);
 
         assertThat(result.size()).isEqualTo(3);
+    }
+
+    @Disabled
+    @Test
+    @DisplayName("JPA 영속성 컨테스트에 shop 엔티티 객체 정보가 없더라도, equals 비교 연산이 성공해야 함")
+    void givenPersistenceContextIsEmpty_whenShopEqualsOperation_thenSuccess() {
+
     }
 }
