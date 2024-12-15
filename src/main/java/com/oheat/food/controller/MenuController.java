dec66fd00653dec4ee00319c320cc297bb413b37
5win <94297900+5win@users.noreply.github.com>
2024-12-15 17:45:42 +0900

    [#7] refact: Rest API URI 변경

--- src/main/java/com/oheat/food/controller/MenuController.java
+++ src/main/java/com/oheat/food/controller/MenuController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/food/menus")
+@RequestMapping("/api/v1/food/shops/menus")
 public class MenuController {
 
     private final MenuService menuService;
