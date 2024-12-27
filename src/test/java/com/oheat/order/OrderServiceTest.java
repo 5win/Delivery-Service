b7f96b288d78fc39e8fc53b14cabad2a98dadff2
5win <94297900+5win@users.noreply.github.com>
2024-12-27 15:01:21 +0900

    [#24] test: 주문 기능 TDD 테스트 명세

---
+++ src/test/java/com/oheat/order/OrderServiceTest.java
@@ -0,0 +1,96 @@
+package com.oheat.order;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+
+    // Create
+    @Disabled
+    @Test
+    @DisplayName("주문 시, 주문 테이블에 사용자 정보, 매장 정보가 저장된다.")
+    void test1() {
+
+    }
+
+    @Disabled
+    @Test
+    @DisplayName("주문 시, 주문 테이블에 주문 상세 정보가 저장된다.")
+    void test2() {
+
+    }
+
+    @Disabled
+    @Test
+    @DisplayName("주문 시, 주문 상태가 pending 상태로 저장된다.")
+    void test3() {
+
+    }
+
+    @Disabled
+    @Test
+    @DisplayName("장바구니에 담긴 메뉴를 주문 메뉴 테이블에 저장한다.")
+    void test4() {
+
+    }
+
+    @Disabled
+    @Test
+    @DisplayName("장바구니에 담긴 메뉴의 옵션 그룹을 주문 옵션 그룹 테이블에 저장한다.")
+    void test5() {
+
+    }
+
+    @Disabled
+    @Test
+    @DisplayName("장바구니에 담긴 메뉴의 옵션을 주문 옵션 테이블에 저장한다.")
+    void test6() {
+
+    }
+
+    // Read
+    @Disabled
+    @Test
+    @DisplayName("주문 내역 조회 시, 각 주문의 매장, 결제 금액, 주문 상태 정보를 Page로 반환한다.")
+    void test7() {
+
+    }
+
+    @Disabled
+    @Test
+    @DisplayName("주문 상세 조회 시, 주문 기본 정보를 반환한다.")
+    void test8() {
+
+    }
+
+    @Disabled
+    @Test
+    @DisplayName("주문 상세 조회 시, 각 주문 항목의 정보를 리스트로 반환한다.")
+    void test9() {
+
+    }
+
+    @Disabled
+    @Test
+    @DisplayName("주문 상세 조회 시, 메뉴 금액, 배달팁, 할인 금액, 총 금액을 반환한다.")
+    void test10() {
+
+    }
+
+    @Disabled
+    @Test
+    @DisplayName("주문 상세 조회 시, 결제 방법을 반환한다.")
+    void test11() {
+
+    }
+
+    // Delete
+    @Disabled
+    @Test
+    @DisplayName("주문 id로 주문 내역을 삭제한다.")
+    void test12() {
+
+    }
+}
