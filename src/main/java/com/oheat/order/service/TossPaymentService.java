fb1afeccb542ae3845617c59b0869323a81a4fee
5win <94297900+5win@users.noreply.github.com>
2025-01-08 17:59:33 +0900

    [#28] fix: 결제 취소 후, Payment 상태 Cancelled로 변경 로직 추가

--- src/main/java/com/oheat/order/service/TossPaymentService.java
+++ src/main/java/com/oheat/order/service/TossPaymentService.java
@@ -4,9 +4,11 @@ import com.oheat.order.dto.PaymentCancelRequest;
 import com.oheat.order.dto.TossPaymentConfirmRequest;
 import com.oheat.order.entity.Payment;
 import com.oheat.order.exception.DuplicatePaymentKeyException;
+import com.oheat.order.exception.PaymentNotExistsException;
 import com.oheat.order.repository.PaymentRepository;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -34,7 +36,12 @@ public class TossPaymentService {
         paymentRepository.save(payment);
     }
 
+    @Transactional
     public void cancel(PaymentCancelRequest cancelRequest) {
+        Payment payment = paymentRepository.findById(cancelRequest.getPaymentKey())
+            .orElseThrow(() -> new PaymentNotExistsException(HttpStatus.BAD_REQUEST, "존재하지 않는 결제입니다."));
+
         tossPaymentClient.cancelPayment(cancelRequest.getPaymentKey(), cancelRequest.getCancelReason());
+        payment.setCancelled();
     }
 }
