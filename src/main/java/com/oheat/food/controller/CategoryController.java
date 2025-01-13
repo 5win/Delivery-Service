2e258d650e37f8a63192b87ee87f637f9dfccaf7
5win <94297900+5win@users.noreply.github.com>
2025-01-13 16:01:18 +0900

    [#22] refact: URI food -> foods로 변경

--- src/main/java/com/oheat/food/controller/CategoryController.java
+++ src/main/java/com/oheat/food/controller/CategoryController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/food/categories")
+@RequestMapping("/api/v1/foods/categories")
 public class CategoryController {
 
     private final CategoryService categoryService;
