6a7adb7c7ad769ba8639de16fe0816ecbd5a9598
5win <94297900+5win@users.noreply.github.com>
2025-01-23 18:24:15 +0900

    [#42] test: smoke test에 지역별 주소 등록 테스트 추가

--- script/SmokeTest.js
+++ script/SmokeTest.js
@@ -64,18 +64,55 @@ export default function () {
 
   sleep(1);
 
-  // 3. 카테고리 조회
+  // 3. 주소 등록
+  const addrUrl = baseUrl + '/users/addresses'
+
+  // 홀수는 서울, 짝수는 부산
+  let latitude, longitude;
+  const sido = (__VU % 2 === 1) ? "서울특별시" : "부산광역시";
+  const sigungu = (__VU % 2 === 1) ? "종로구" : "해운대구";
+
+  if (sido === "서울") {
+    latitude = (Math.random() * (37.7 - 37.4)) + 37.4; // 37.4 ~ 37.7
+    longitude = (Math.random() * (127.2 - 126.8)) + 126.8; // 126.8 ~ 127.2
+  } else {
+    latitude = (Math.random() * (35.3 - 35.0)) + 35.0; // 35.0 ~ 35.3
+    longitude = (Math.random() * (129.2 - 128.8)) + 128.8; // 128.8 ~ 129.2
+  }
+
+  const addrPayload = JSON.stringify({
+    address: `${sido} ${sigungu}`,
+    detailAddress: "상세 주소",
+    sido: sido,
+    sigungu: sigungu,
+    latitude: latitude,
+    longitude: longitude,
+    nickname: `${__VU}'s Home`,
+    selected: true
+  });
+
+  const addrRegParams = {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + jwt
+    }
+  }
+
+  const addrRes = http.post(addrUrl, addrPayload, addrRegParams)
+
+  sleep(1);
+
+  // 4. 카테고리 조회
   const categoryUrl = baseUrl + '/foods/categories';
   const categoryRes = http.get(categoryUrl);
 
   check(categoryRes, {
-    '카테고리 조회 성공': (res) => res.status === 200,
-    '치킨 카테고리 존재': (res) => JSON.parse(res.body)[0]?.name === '치킨'
+    '카테고리 조회 성공': (res) => res.status === 200 && JSON.parse(res.body)[0]?.name === '치킨',
   });
 
   sleep(1);
 
-  // 4. 카테고리로 최신 등록 순 매장 조회
+  // 5. 카테고리로 최신 등록 순 매장 조회
   let category = encodeURIComponent('치킨');
   let sort = encodeURIComponent('id');
   let page = encodeURIComponent(0);
@@ -85,30 +122,25 @@ export default function () {
 
   check(findShopOrderByIdRes, {
     '최신 등록순 매장 조회 성공': (res) => res.status === 200,
-    '조회된 페이지 사이즈 10': (res) => JSON.parse(res.body)?.content?.length === 10
   });
 
   sleep(1);
 
-  // 5. 가까운 거리 순 매장 조회
+  // 6. 가까운 거리 순 매장 조회
   sort = encodeURIComponent('distance,id');
-  const latitude = (Math.random() * (38.6 - 33.0)) + 33.0;
-  const longitude = (Math.random() * (131.0 - 124.0)) + 124.0;
   const findShopOrderByDistRes = http.get(baseUrl
       + `/foods/shops?category=${category}&sort=${sort}&latitude=${latitude}&longitude=${longitude}&page=${page}&size=${size}`);
 
   check(findShopOrderByDistRes, {
     '가까운 거리 순 매장 조회 성공': (res) => res.status === 200,
-    '조회된 페이지 사이즈 10': (res) => JSON.parse(res.body)?.content?.length === 10
   });
 
   sleep(1);
 
-  // 6. VU 번호에 해당하는 id 매장의 메뉴 그룹 및 메뉴 조회
+  // 7. VU 번호에 해당하는 id 매장의 메뉴 그룹 및 메뉴 조회
   const menuGroupRes = http.get(baseUrl + `/foods/shops/menu-groups/${__VU}`);
 
   check(menuGroupRes, {
     '메뉴 그룹 조회 성공': (res) => res.status === 200,
-    '메뉴 그룹의 메뉴 개수 10개': (res) => JSON.parse(res.body)[0]?.menuList.length === 10
   })
 }
